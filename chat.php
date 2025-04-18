8c3c5b72cea4716b499716acc57575ef41d3f98f
Soham <rimatip472@operades.com>
2025-04-18 10:14:45 +0530

    modified few things

--- chat.php
+++ chat.php
@@ -61,7 +61,7 @@ if ($selected_user_id) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chat - Chat App</title>
+    <title>LetsTalk - WT Lab Project</title>
     <link rel="stylesheet" href="style.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
